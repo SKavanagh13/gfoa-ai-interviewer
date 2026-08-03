380cebe1daf1cafa7df8ceb3b2df80aaccfb080c
SKavanagh13 <skavanagh@gfoa.org>
2026-08-03 18:30:39 -0500

    tune answer turn taking (#31)

--- app/interview/created/live-session-client.tsx
+++ app/interview/created/live-session-client.tsx
@@ -334,10 +334,11 @@ export function LiveSessionClient({
             There are six big questions the interview will cover. Select Start
             interview and wait for the interviewer to begin. It will ask you the
             six questions one at a time. It may have follow-up questions on your
-            answers. Our interviewer can be curious, and although we have asked
-            it to restrain its enthusiasm, if you feel it is asking too many
-            follow-ups, feel free to say, &quot;we are done with this question,
-            let&apos;s move on.&quot;
+            answers. The interviewer has been asked to pause for a second or two
+            to make sure you are done talking before moving on. Our interviewer
+            can be curious, and although we have asked it to restrain its
+            enthusiasm, if you feel it is asking too many follow-ups, feel free
+            to say, &quot;we are done with this question, let&apos;s move on.&quot;
           </p>
         </div>
       )}
